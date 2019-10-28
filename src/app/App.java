4890fd62cb807b0ff834e10a907dc263dd0c0bb2
MICHELE MASTROIANNI <mastroiannim@PALEOCAPA.DID>
2019-10-28 20:54:07 +0100

    parametri in servlet

--- src/app/App.java
+++ src/app/App.java
@@ -3,6 +3,7 @@ package app;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,18 +20,35 @@ public class App extends HttpServlet{
     }
 
     private String mymsg;
-    public void init() throws ServletException 
-    {      
-       mymsg = "Http Servlet Demo";   
+    public void init(ServletConfig config) throws ServletException 
+    {     
+       super.init(config);
+       mymsg = config.getInitParameter("title");
+       //mymsg = Constants.TITOLO;   
     }
 
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        String p = req.getHeader("p");
+        
         // Setting up the content type of web page      
         res.setContentType("text/html");
         // Writing the message on the web page      
-        PrintWriter out = res.getWriter();      
-        out.println("<h1>" + mymsg + "</h1>");      
-        out.println("<p>" + "Hello Friends!" + "</p>");
+        PrintWriter out = res.getWriter(); 
+        String value = req.getParameter("title");
+        if(value == null){
+            value = mymsg; 
+        }
+        out.println("<h1>" + value + "</h1>");      
+        out.println("<p>" + "Hello Friends!" + p + "</p>");
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        // Setting up the content type of web page      
+        res.setContentType("text/html");
+        // Writing the message on the web page      
+        String value = req.getParameter("title");
+        PrintWriter out = res.getWriter();  
+        out.println("post ok!" + value);
     }
 
 }
